73d7224e50463c12359dda0b9ea2283341587849
nhopo <thanh.le.nguyen@rwth-aachen.de>
2023-01-19 19:16:15 +0100

    Complete check win functionality and rearrange functions

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ const Player = (symbol) => {
 const gameBoard = (() => {
     
     let _markList = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    let _lastPlayer;
 
+    const setLastPlayer = player => {
+        _lastPlayer = player;
+    }
+    const getLastPlayer = () => _lastPlayer;
+    
     const displayBoard = () => {
         let i = 0;
         grids.forEach(grid => {
@@ -16,9 +22,7 @@ const gameBoard = (() => {
             i++;
         });
     }
-
     const getMarkList = () => _markList;
-
     const addMark = (symbol, index) => {
         _markList[index] = symbol;
     }
@@ -28,39 +32,81 @@ const gameBoard = (() => {
     const isFull = () => {
         return _markList.includes(" ") ? false : true;
     }
+
+    /*  Check winning positions:
+        Row:         [1 2 3], [4 5 6], [7 8 9]
+        Column:      [1 4 7], [2 5 8], [3 6 9]
+        Diagonal:    [1 5 9], [3 5 7]
+     */
+    const _checkRowWin = () => {
+        for(let i = 0; i < 7; i += 3){
+            let temp = _markList.slice(i, i + 3);
+            const sum = temp.reduce((accumulator,current) => accumulator + current);
+            
+            if (sum == 'xxx') {
+                return 'x';
+            } else if (sum == 'ooo') {
+                return 'o';
+            }
+        }
+        return false;
+    }
+    const _checkColumnWin = () => {
+        for(let i = 0; i < 3; i++) {
+            let sum = _markList[i] + _markList[i+3] + _markList[i+6];
+            if (sum == 'xxx') {
+                return 'x';
+            } else if (sum == 'ooo') {
+                return 'o';
+            }
+        } 
+        return false;
+    }
+    const _checkDiagWin = () => {
+        
+        let sum_1 = _markList[0] + _markList[4] + _markList[8];
+        let sum_2 = _markList[2] + _markList[4] + _markList[6];
+        if (sum_1 == 'xxx' || sum_2 == 'xxx'){
+            return 'x';
+        } else if (sum_1 == 'ooo' || sum_2 == 'ooo') {
+            return 'o';
+        } else {
+            return false;
+        }
+    }
+    const gameOver = () => {
+        let rowWin = _checkRowWin();
+        let columnWin = _checkColumnWin();
+        let diagWin = _checkDiagWin();
+        
+        if (gameBoard.isFull()){
+            return 'Game ends in a tie.'
+        } else if (rowWin){
+            return `Player ${rowWin} wins.`;
+        } else if (columnWin){
+            return `Player ${columnWin} wins.`;
+        } else if (diagWin) {
+            return `Player ${diagWin} wins.`;
+        } else {
+            return false;
+        }
+    }
     
-    return {displayBoard, isMarked, addMark, isFull, getMarkList}
+    return {
+        displayBoard,
+        setLastPlayer,
+        getLastPlayer,
+        isMarked,
+        addMark,
+        isFull,
+        getMarkList,
+        gameOver
+    }
     
 })();
 
 const displayController = (() => {
     
-    let _lastPlayer;
-    let _winner;
-    
-    const setLastPlayer = player => {
-        _lastPlayer = player;
-    }
-    const getLastPlayer = () => _lastPlayer;
-    
-    // Gewinnsituationen: [1 2 3], [4 5 6], [7 8 9], 
-        // [1 4 7], [2 5 8], [3 6 9]
-        // [1 5 9], [3 5 7], 
-    const checkRowWin = (markList) => {
-        
-
-        for(let i = 0; i < 7; i += 3){
-            let temp = markList.slice(i, i + 3);
-            const sum = temp.reduce((accumulator,current) => accumulator + current);
-            if (sum == 'xxx') {
-                _winner = 'x';
-            } else if (sum == 'ooo') {
-                _winner = 'o';
-            }
-        }
-    }
-    return {setLastPlayer, getLastPlayer, checkRowWin}
-
 })();
 
 
@@ -68,21 +114,18 @@ grids.forEach(grid => grid.addEventListener('click', () => {
 
     let index = Array.prototype.indexOf.call(grids, grid);
     if (!gameBoard.isMarked(index)){
-        switch (displayController.getLastPlayer().symbol) {
+        switch (gameBoard.getLastPlayer().symbol) {
             case 'o': 
                 gameBoard.addMark('x', index); 
-                displayController.setLastPlayer(player_x);
+                gameBoard.setLastPlayer(player_x);
                 break;
             case 'x': 
                 gameBoard.addMark('o', index);
-                displayController.setLastPlayer(player_o);
+                gameBoard.setLastPlayer(player_o);
                 break;
         };
         gameBoard.displayBoard();
-        displayController.checkRowWin(gameBoard.getMarkList());
-        if (gameBoard.isFull()){
-            console.log("Game over")
-        }
+        console.log(gameBoard.gameOver());
     }
     
 }));
@@ -92,5 +135,5 @@ gameBoard.displayBoard();
 const player_o = Player('o');
 const player_x = Player('x');
 
-displayController.setLastPlayer(player_o);
+gameBoard.setLastPlayer(player_o);
 
